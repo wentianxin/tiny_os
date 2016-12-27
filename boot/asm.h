c8acbf71c5aea5aa7b8bf770c243f0d2fe0bf619
tisong <tianxin675470193@outlook.com>
2016-12-27 12:10:20 +0800

    boot启动段描述符的定义与描述

--- boot/asm.h
+++ boot/asm.h
@@ -68,21 +68,22 @@
 
 
 /* Segment types: Ex Dc RW Ac */
-#define STA_X 8  // 如果是1，则在段中的代码是可执行的；如果是0，则是数据选择子（ds,ss,es,fs,gs）
-#define STA_C 4   
+#define STA_X 8  // 如果是1，则在段中的代码是可执行的；如果是0，则是数据选择子（ds,ss,es,fs,gs） executeable
+#define STA_C 4  //
 #define STA_E 4
 #define STA_R 2
 #define STA_W 2
 #define STA_A 1
 
-#define SEG_NULLASM
-	.word 0, 0;
-	.byte 0, 0, 0, 0;
+// GDT 第一项都是NULL
+#define SEG_NULLASM                                             \
+    .word 0, 0;                                                 \
+    .byte 0, 0, 0, 0
 
-#define SEG_ASM(type, base, lim)
-	.word (lim) & 0xffff, (base) & 0xffff;
-	.byte ((base) >> 16) & 0xff, (0x90 | (type)) & 0xff,
-		  (0xC0 | (lim >> 16)) & 0xff, (base >> 24) & 0xff;
+#define SEG_ASM(type,base,lim)                                  \
+    .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);          \
+    .byte (((base) >> 16) & 0xff), (0x90 | (type)),             \
+        (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
 
 
 #endif
